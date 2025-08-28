7562a3e1475f7deb22e319e67ac681b4b0fd6da7
SURYA <ncsurya2005@gmail.com>
2025-08-28 22:17:42 +0530

    finished task

--- js/index.js
+++ js/index.js
@@ -155,3 +155,523 @@ const fortyFiveconst = "umma";
 const fortySixconst = "vainko ";
 const fortySevenconst = 88;
 const fortyEightconst = 242424242;
+
+
+//var initialization
+
+oneVar = 1;
+twoVar = 2;
+threeVar = 3;
+fourVar = 4;
+fiveVar = 5;
+sixVar = 6;
+sevenVar = 7;
+eightVar = 8;
+nineVar = 9;
+tenVar = 10;
+elevenVar = 11;
+twelveVar = 12;
+thirteenVar =13;
+fourteenVar =14;
+fifteenVar =15;
+sixteenvar =16;
+seventeenVar =17;
+eighteenVar =18;
+nineteenVar =19;
+twentyVar =20;
+twentyoneVar =21;
+twentytwoVar =22;
+twentyThreeVar =23;
+twentyFourVar =24;
+twentyFiveVar =25;
+twentySixVar =26;
+twentySevenVar =27;
+twentyEightVar =28;
+twentyNineVar =29;
+thirtyVar =30;
+thirtyOneVar =31;
+thirtyTwoVar =32;
+thirtyThreeVar =33;
+thirtyFourVar =34;
+thirtyfiveVar =35;
+thirtySixVar =36;
+thirtySevenVar =37;
+thirtyEightVar =38;
+thirtyNineVar =39;
+fortyVar =40;
+fortyOneVar =41;
+fortyTwoVar =42;
+fortyThreeVar =43;
+fortyFourVar =44;
+fortyFiveVar =45;
+fortySixVar =46;
+fortySevenVar =47;
+fortyEightVar =48;
+
+//let initialization
+
+
+onelet = 1;
+twolet = 2;
+threelet = 3;
+fourlet = 4;
+fivelet = 5;
+sixlet = 6;
+sevenlet = 7;
+eightlet = 8;
+ninelet = 9;
+tenlet = 10;
+elevenlet = 11;
+twelvelet = 12;
+thirteenlet =13;
+fourteenlet =14;
+fifteenlet =15;
+sixteenlet =16;
+seventeenlet =17;
+eighteenlet =18;
+nineteenlet =19;
+twentylet =20;
+twentyonelet =21;
+twentytwolet =22;
+twentyThreelet =23;
+twentyFourlet =24;
+twentyFivelet =25;
+twentySixlet =26;
+twentySevenlet =27;
+twentyEightlet =28;
+twentyNinelet =29;
+thirtylet =30;
+thirtyOnelet =31;
+thirtyTwolet =32;
+thirtyThreelet =33;
+thirtyFourlet =34;
+thirtyfivelet =35;
+thirtySixlet =36;
+thirtySevenlet =37;
+thirtyEightlet =38;
+thirtyNinelet =39;
+fortylet =40;
+fortyOnelet =41;
+fortyTwolet =42;
+fortyThreelet =43;
+fortyFourlet =44;
+fortyFivelet =45;
+fortySixlet =46;
+fortySevenlet =47;
+fortyEightlet =48;
+
+ //array initializations
+
+
+ let arrayarrayarrayarray1 = [];
+let arrayarrayarrayarray2 = [1, 2, 3, 4, 5];
+let arrayarrayarrayarray3 = [10, 20, 30, 40, 50];
+let arrayarrayarrayarray4 = [100, 200, 300];
+let arrayarrayarrayarray5 = [7, 14, 21, 28];
+let arrayarrayarrayarray6 = [2, 4, 6, 8, 10];
+let arrayarrayarrayarray7 = [1, 3, 5, 7, 9];
+let arrayarrayarrayarray8 = [11, 22, 33];
+let arrayarrayarrayarray9 = [9, 8, 7, 6, 5];
+let arrayarrayarrayarray10 = [0, 0, 0, 0];
+let arrayarrayarrayarray11 = [1];
+let arrayarrayarrayarray12 = [2];
+let arrayarrayarrayarray13 = [3];
+let arrayarrayarrayarray14 = [4];
+let arrayarrayarrayarray15 = [5];
+let arrayarrayarrayarray16 = [6];
+let arrayarrayarrayarray17 = [7];
+let arrayarrayarrayarray18 = [8];
+let arrayarrayarrayarray19 = [9];
+let arrayarrayarrayarray20 = [10];
+let arrayarrayarrayarray21 = [1.1, 2.2, 3.3];
+let arrayarrayarrayarray22 = [0.5, 1.5, 2.5];
+let arrayarrayarrayarray23 = [-1, -2, -3];
+let arrayarrayarrayarray24 = [-10, -20, -30];
+let arrayarrayarrayarray25 = [1000, 2000, 3000];
+let arrayarrayarrayarray26 = [111, 222, 333];
+let arrayarrayarrayarray27 = [12, 24, 36, 48];
+let arrayarrayarrayarray28 = [5, 10, 15, 20];
+let arrayarrayarrayarray29 = [13, 26, 39];
+let arrayarrayarrayarray30 = [99, 88, 77];
+let arrayarrayarrayarray31 = [123, 456, 789];
+let arrayarrayarrayarray32 = [25, 50, 75, 100];
+let arrayarrayarrayarray33 = [6, 12, 18, 24];
+let arrayarrayarrayarray34 = [8, 16, 24, 32];
+let arrayarrayarrayarray35 = [9, 18, 27, 36];
+let arrayarrayarrayarray36 = [101, 202, 303];
+let arrayarrayarrayarray37 = [2, 3, 5, 7, 11];
+let arrayarrayarrayarray38 = [4, 9, 16, 25];
+let arrayarrayarrayarray39 = [64, 81, 100];
+let arrayarrayarrayarray40 = [15, 30, 45, 60];
+let arrayarrayarrayarray41 = [14, 28, 42, 56];
+let arrayarrayarrayarray42 = [70, 80, 90, 100];
+let arrayarrayarrayarray43 = [1, 10, 100, 1000];
+let arrayarrayarrayarray44 = [3, 6, 9, 12];
+let arrayarrayarrayarray45 = [20, 40, 60, 80];
+let arrayarrayarrayarray46 = [99, 66, 33];
+let arrayarrayarrayarray47 = [55, 65, 75, 85];
+let arrayarrayarrayarray48 = [7, 77, 777];
+
+
+
+//object initializations
+
+let obj1 = {};
+let obj2 = { a: 1, b: 2 };
+let obj3 = { x: 10, y: 20, z: 30 };
+let obj4 = { id: 101, name: "John" };
+let obj5 = { price: 500, qty: 3 };
+let obj6 = { roll: 1, grade: "A" };
+let obj7 = { age: 25, city: "Chennai" };
+let obj8 = { brand: "Nike", size: 9 };
+let obj9 = { width: 100, height: 200 };
+let obj10 = { p: 2, q: 4, r: 6 };
+let obj11 = { alpha: 0.1, beta: 0.2 };
+let obj12 = { one: 1 };
+let obj13 = { two: 2 };
+let obj14 = { three: 3 };
+let obj15 = { four: 4 };
+let obj16 = { five: 5 };
+let obj17 = { six: 6 };
+let obj18 = { seven: 7 };
+let obj19 = { eight: 8 };
+let obj20 = { nine: 9 };
+let obj21 = { flag: true, value: 42 };
+let obj22 = { marks: [80, 90, 95] };
+let obj23 = { id: 201, active: false };
+let obj24 = { username: "surya", password: "1234" };
+let obj25 = { score: 1000, level: 5 };
+let obj26 = { lat: 13.0827, lng: 80.2707 };
+let obj27 = { radius: 15, diameter: 30 };
+let obj28 = { month: "March", days: 31 };
+let obj29 = { weekday: "Monday", weekend: false };
+let obj30 = { start: 9, end: 17 };
+let obj31 = { country: "India", code: "IN" };
+let obj32 = { email: "test@example.com" };
+let obj33 = { phone: 9876543210 };
+let obj34 = { empId: 1001, dept: "IT" };
+let obj35 = { status: "active", progress: 75 };
+let obj36 = { version: "1.0", stable: true };
+let obj37 = { a: { b: 2 } };
+let obj38 = { nested: { x: 1, y: 2 } };
+let obj39 = { fruits: ["apple", "mango"] };
+let obj40 = { prime: [2, 3, 5, 7] };
+let obj41 = { rollNo: 45, section: "B" };
+let obj42 = { marks: { maths: 90, eng: 85 } };
+let obj43 = { key: "value" };
+let obj44 = { pi: 3.14, e: 2.71 };
+let obj45 = { length: 25, breadth: 10 };
+let obj46 = { min: 0, max: 100 };
+let obj47 = { speed: 60, unit: "km/h" };
+let obj48 = { power: 220, voltage: 440 };
+
+
+//array of objects initialization
+
+let arr1 = [{ a: 1 }, { b: 2 }, { c: 3 }];
+let arr2 = [{ x: 10 }, { y: 20 }, { z: 30 }];
+let arr3 = [{ id: 101 }, { id: 102 }, { id: 103 }];
+let arr4 = [{ name: "John" }, { name: "Mike" }, { name: "Sara" }];
+let arr5 = [{ price: 100 }, { price: 200 }, { price: 300 }];
+let arr6 = [{ roll: 1 }, { roll: 2 }, { roll: 3 }];
+let arr7 = [{ age: 25 }, { age: 30 }, { age: 35 }];
+let arr8 = [{ brand: "Nike" }, { brand: "Adidas" }, { brand: "Puma" }];
+let arr9 = [{ width: 100 }, { width: 200 }, { width: 300 }];
+let arr10 = [{ p: 2 }, { q: 4 }, { r: 6 }, { s: 8 }];
+let arr11 = [{ alpha: 0.1 }, { beta: 0.2 }, { gamma: 0.3 }];
+let arr12 = [{ one: 1 }, { two: 2 }, { three: 3 }];
+let arr13 = [{ four: 4 }, { five: 5 }, { six: 6 }];
+let arr14 = [{ seven: 7 }, { eight: 8 }, { nine: 9 }];
+let arr15 = [{ id: 201 }, { id: 202 }, { id: 203 }];
+let arr16 = [{ value: 10 }, { value: 20 }, { value: 30 }];
+let arr17 = [{ marks: 80 }, { marks: 90 }, { marks: 100 }];
+let arr18 = [{ score: 50 }, { score: 60 }, { score: 70 }];
+let arr19 = [{ month: "Jan" }, { month: "Feb" }, { month: "Mar" }];
+let arr20 = [{ day: "Mon" }, { day: "Tue" }, { day: "Wed" }];
+let arr21 = [{ city: "Chennai" }, { city: "Delhi" }, { city: "Mumbai" }];
+let arr22 = [{ country: "India" }, { country: "USA" }, { country: "UK" }];
+let arr23 = [{ phone: 111 }, { phone: 222 }, { phone: 333 }];
+let arr24 = [{ empId: 1001 }, { empId: 1002 }, { empId: 1003 }];
+let arr25 = [{ lat: 13.0 }, { lat: 14.0 }, { lat: 15.0 }];
+let arr26 = [{ lng: 80.0 }, { lng: 81.0 }, { lng: 82.0 }];
+let arr27 = [{ radius: 5 }, { radius: 10 }, { radius: 15 }];
+let arr28 = [{ length: 25 }, { length: 50 }, { length: 75 }];
+let arr29 = [{ height: 150 }, { height: 160 }, { height: 170 }];
+let arr30 = [{ weight: 60 }, { weight: 70 }, { weight: 80 }];
+let arr31 = [{ subject: "Maths" }, { subject: "Science" }, { subject: "English" }];
+let arr32 = [{ grade: "A" }, { grade: "B" }, { grade: "C" }];
+let arr33 = [{ section: "X" }, { section: "Y" }, { section: "Z" }];
+let arr34 = [{ key: "k1" }, { key: "k2" }, { key: "k3" }];
+let arr35 = [{ val: 111 }, { val: 222 }, { val: 333 }];
+let arr36 = [{ user: "u1" }, { user: "u2" }, { user: "u3" }];
+let arr37 = [{ code: "C1" }, { code: "C2" }, { code: "C3" }];
+let arr38 = [{ pin: 123 }, { pin: 456 }, { pin: 789 }];
+let arr39 = [{ zip: 600001 }, { zip: 600002 }, { zip: 600003 }];
+let arr40 = [{ num: 1 }, { num: 2 }, { num: 3 }, { num: 4 }];
+let arr41 = [{ roll: 10 }, { roll: 20 }, { roll: 30 }, { roll: 40 }];
+let arr42 = [{ start: 9 }, { mid: 12 }, { end: 15 }];
+let arr43 = [{ min: 0 }, { mid: 50 }, { max: 100 }];
+let arr44 = [{ speed: 30 }, { speed: 60 }, { speed: 90 }];
+let arr45 = [{ power: 220 }, { power: 330 }, { power: 440 }];
+let arr46 = [{ voltage: 110 }, { voltage: 220 }, { voltage: 330 }];
+let arr47 = [{ status: "active" }, { status: "inactive" }, { status: "pending" }];
+let arr48 = [{ level: 1 }, { level: 2 }, { level: 3 }, { level: 4 }];
+
+
+
+//named functions
+
+function sayHello() {
+  console.log("Hello");
+}
+
+function daay() {
+  console.log("Welcome User!");
+}
+
+function printName() {
+  console.log("My name is Surya");
+}
+
+function addNumbers() {
+  console.log(5 + 10);
+}
+
+function multiplyNumbers() {
+  console.log(3 * 4);
+}
+
+function subtractNumbers() {
+  console.log(20 - 5);
+}
+
+function divideNumbers() {
+  console.log(20 / 4);
+}
+
+function squareNumber() {
+  console.log(5 * 5);
+}
+
+function cubeNumber() {
+  console.log(3 * 3 * 3);
+}
+
+function printToday() {
+  console.log("Today is a great day!");
+}
+
+function sayGoodMorning() {
+  console.log("Good Morning!");
+}
+
+function sayGoodNight() {
+  console.log("Good Night!");
+}
+
+function isEven() {
+  console.log(10 % 2 === 0);
+}
+
+function isOdd() {
+  console.log(7 % 2 !== 0);
+}
+
+function printArray() {
+  console.log([1, 2, 3, 4, 5]);
+}
+
+function printObject() {
+  console.log({ name: "John", age: 25 });
+}
+
+function printBoolean() {
+  console.log(true);
+}
+
+function printNull() {
+  console.log(null);
+}
+
+function printUndefined() {
+  console.log(undefined);
+}
+
+function printMessage() {
+  console.log("This is a simple JavaScript function!");
+}
+
+
+// anonymous functions 
+
+let sayHello = function() {
+  console.log("Hello");
+};
+
+let greet = function() {
+  console.log("Welcome User!");
+};
+
+let printName = function() {
+  console.log("My name is Surya");
+};
+
+let addNumbers = function() {
+  console.log(5 + 10);
+};
+
+let multiplyNumbers = function() {
+  console.log(3 * 4);
+};
+
+let subtractNumbers = function() {
+  console.log(20 - 5);
+};
+
+let divideNumbers = function() {
+  console.log(20 / 4);
+};
+
+let squareNumber = function() {
+  console.log(5 * 5);
+};
+
+let cubeNumber = function() {
+  console.log(3 * 3 * 3);
+};
+
+let printToday = function() {
+  console.log("Today is a great day!");
+};
+
+let sayGoodMorning = function() {
+  console.log("Good Morning!");
+};
+
+let sayGoodNight = function() {
+  console.log("Good Night!");
+};
+
+let isEven = function() {
+  console.log(10 % 2 === 0);
+};
+
+let isOdd = function() {
+  console.log(7 % 2 !== 0);
+};
+
+let printArray = function() {
+  console.log([1, 2, 3, 4, 5]);
+};
+
+let printObject = function() {
+  console.log({ name: "John", age: 25 });
+};
+
+let printBoolean = function() {
+  console.log(true);
+};
+
+let printNull = function() {
+  console.log(null);
+};
+
+let printUndefined = function() {
+  console.log(undefined);
+};
+
+let printMessage = function() {
+  console.log("This is a simple JavaScript function!");
+};
+let Hi = function() {
+  console.log("Hi there!");
+};
+
+let Bye = function() {
+  console.log("Goodbye!");
+};
+
+let welcome = function() {
+  console.log("Welcome to JavaScript!");
+};
+
+let thanks = function() {
+  console.log("Thank you!");
+};
+
+let Ten = function() {
+  console.log(10);
+};
+
+let Hundred = function() {
+  console.log(100);
+};
+
+let Thousand = function() {
+  console.log(1000);
+};
+
+let True = function() {
+  console.log(true);
+};
+
+let False = function() {
+  console.log(false);
+};
+
+let Nothing = function() {
+  console.log("Nothing here!");
+};
+
+
+
+// arrow functions
+
+let sayHello = () => console.log("Hello");
+let greetUser = () => console.log("Welcome User!");
+let printName = () => console.log("My name is Surya");
+let addNumbers = () => console.log(5 + 10);
+let multiplyNumbers = () => console.log(3 * 4);
+let subtractNumbers = () => console.log(20 - 5);
+let divideNumbers = () => console.log(20 / 4);
+let squareNumber = () => console.log(5 * 5);
+let cubeNumber = () => console.log(3 * 3 * 3);
+let printToday = () => console.log("Today is a great day!");
+let sayGoodMorning = () => console.log("Good Morning!");
+let sayGoodNight = () => console.log("Good Night!");
+let isEven = () => console.log(10 % 2 === 0);
+let isOdd = () => console.log(7 % 2 !== 0);
+let printArray = () => console.log([1, 2, 3, 4, 5]);
+let printObject = () => console.log({ name: "John", age: 25 });
+let printBoolean = () => console.log(true);
+let printNull = () => console.log(null);
+let printUndefined = () => console.log(undefined);
+let printMessage = () => console.log("This is a simple JS arrow function!");
+let sayHi = () => console.log("Hi there!");
+let sayBye = () => console.log("Goodbye!");
+let welcomeMessage = () => console.log("Welcome to JavaScript!");
+let thankYou = () => console.log("Thank you!");
+let printTen = () => console.log(10);
+let printHundred = () => console.log(100);
+let printThousand = () => console.log(1000);
+let showTrue = () => console.log(true);
+let showFalse = () => console.log(false);
+let showNothing = () => console.log("Nothing here!");
+let printOne = () => console.log(1);
+let printTwo = () => console.log(2);
+let printThree = () => console.log(3);
+let printFour = () => console.log(4);
+let printFive = () => console.log(5);
+let doubleNumber = () => console.log(2 * 2);
+let tripleNumber = () => console.log(3 * 3);
+let printYes = () => console.log("Yes");
+let printNo = () => console.log("No");
+let printOk = () => console.log("OK");
+let printFine = () => console.log("I am fine");
+let printAwesome = () => console.log("Awesome!");
+let showArray = () => console.log([10, 20, 30]);
+let showObject = () => console.log({ id: 1, title: "Book" });
+let showNull = () => console.log(null);
+let showUndefined = () => console.log(undefined);
+let printDone = () => console.log("Done!");
+let printExit = () => console.log("Exit Program");
